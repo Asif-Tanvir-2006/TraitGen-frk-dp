1511f43728fd6de60d5985b81de76b4f0cbb9910
Prayas <ralphraout@gmail.com>
2026-09-16 17:06:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def main(args):
             logger.info(f"Epoch {epoch}: Train Loss={train_loss:.4f}, Train Acc={train_acc:.4f}, ")
 
             if epoch == args.epochs - 1:
-                val_loss, val_acc = validate(args, model, test_loader, device)
+                # val_loss, val_acc = validate(args, model, test_loader, device)
                 checkpoint_path = os.path.join(args.output_dir, "best_model.pth")
                 # Save model.module to strip the 'module.' wrapper prefix
                 save_checkpoint(checkpoint_path, model.module, optimizer, epoch)
