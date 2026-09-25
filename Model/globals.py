699ab1bca22789cb1bdb9ad32e1d5493471df15e
asif <asiftanvir2006@gmail.com>
2026-09-26 00:40:06 +0530

    update

---
+++ Model/globals.py
@@ -0,0 +1,3 @@
+
+
+IMAGE_DIR = '/kaggle/input/datasets/wenewone/cub2002011/CUB_200_2011/images'
---
+++ Model/test_VE.py
@@ -0,0 +1,14 @@
+import VE
+import bioclip_VE
+
+
+
+bioClip = bioclip_VE.BioCLIP()
+ve = VE.VisionEncoder(bioClip)
+
+image_list =[
+    '/kaggle/input/datasets/wenewone/cub2002011/CUB_200_2011/images/001.Black_footed_Albatross/Black_Footed_Albatross_0001_796111.jpg',
+    '/kaggle/input/datasets/wenewone/cub2002011/CUB_200_2011/images/001.Black_footed_Albatross/Black_Footed_Albatross_0003_796136.jpg'
+]
+
+print(ve.forward(image_list))
