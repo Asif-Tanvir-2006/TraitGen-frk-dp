7f17744d1d0e8588f9830b1b6d3542cec611e5d2
asif <asiftanvir2006@gmail.com>
2026-09-26 00:54:18 +0530

    update

--- Model/test_VE.py
+++ Model/test_VE.py
@@ -8,7 +8,8 @@ ve = VE.VisionEncoder(bioClip)
 
 image_list =[
     '/kaggle/input/datasets/wenewone/cub2002011/CUB_200_2011/images/001.Black_footed_Albatross/Black_Footed_Albatross_0001_796111.jpg',
-    '/kaggle/input/datasets/wenewone/cub2002011/CUB_200_2011/images/001.Black_footed_Albatross/Black_Footed_Albatross_0003_796136.jpg'
+    '/kaggle/input/datasets/wenewone/cub2002011/CUB_200_2011/images/001.Black_footed_Albatross/Black_Footed_Albatross_0001_796111.jpg'
+    # '/kaggle/input/datasets/wenewone/cub2002011/CUB_200_2011/images/001.Black_footed_Albatross/Black_Footed_Albatross_0003_796136.jpg'
 ]
 
 print(ve.forward(image_list))
