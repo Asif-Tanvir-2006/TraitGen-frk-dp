81d2b95684e95a58773b0dec9f3f46143bf01745
asif <asiftanvir2006@gmail.com>
2026-09-26 00:42:11 +0530

    update

--- Model/VE.py
+++ Model/VE.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 
 
-class VisionEncoder(nn.module):
+class VisionEncoder(nn.Module):
     def __init_(self, model):
         self.model = model
-    
+
     
     
     def forward(self, *args, **kwargs):
@@ -13,4 +13,3 @@ class VisionEncoder(nn.module):
         #expects B number of image paths, produces B*P*D matrix, 
         # where P is the number of patches produced per image and 
         # D is the number of dimensions in each patch     
-    
